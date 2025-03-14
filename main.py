6882678c84a1db5aef0f674f9a0c629f8bfa375a
seyedmm <seyedmm044@protonmail.com>
2025-03-15 00:06:55 +0330

    added main.py

--- main.py
+++ main.py
@@ -1,6 +1,34 @@
+import json
+import requests
 import util
 import time
+from os import getenv
+from dotenv import load_dotenv
+load_dotenv()
 
-DRIVER_LIST = util.session_driver_list(9686)
-pos_list = util.get_last_drivers_position(9686, DRIVER_LIST)
-print(util.pretty_output(pos_list))
+start_time = time.time()
+chat_id = 1213079698
+msg_id = 3
+BASE_URL = f"https://api.telegram.org/bot{getenv("TG_TOKEN")}/"
+current_session = util.get_last_session()
+DRIVER_LIST = util.session_driver_list(current_session['session_key'])
+
+class TelegramUpdateBot:
+    def __init__(self, chat_id: int):
+        self.chat_id=chat_id
+        self.current_message_id=None
+        self.last_new_message_time=0
+        self.last_update_time=0
+        self.current_session={}
+        self.driver_list=[]
+        self.position_list=[]
+
+    def update_message(self):
+        resp = requests.get(BASE_URL+f"updateMessage?chat_id={self.chat_id}&message_id={self.current_message_id}&text={util.pretty_output(self.position_list)}", proxies=util.PROXIES)
+        return json.loads(resp.text)
+
+    def reset_session_and_drivers(self):
+        self.current_session_key = util.get_last_session()
+        self.driver_list = util.session_driver_list(self.current_session['session_key'])
+    
+
