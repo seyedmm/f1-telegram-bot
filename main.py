c8bde4a95d75a672dd10a685f011f8edee1ad5bc
seyedmm <seyedmm044@protonmail.com>
2025-03-15 06:49:56 +0330

    updated main.py

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
 import asyncio
+from contextlib import asynccontextmanager
+from datetime import datetime
 from os import getenv
-from aioclock import AioClock, Depends, Every
+from aioclock import AioClock, Every
 from dotenv import load_dotenv
 from models import TelegramUpdateBot
 load_dotenv()
 
 tg_bot = TelegramUpdateBot(chat_id=int(getenv("CHAT_ID")))
-app = AioClock()
 
 def return_tg_bot():
     global tg_bot
     return tg_bot
 
+@asynccontextmanager
+async def lifespan(app: AioClock):
+    global tg_bot
+    print("startup")
+    tg_bot.fetch_session_and_drivers()
+    tg_bot.send_new_message()
+    yield app
+    tg_bot.tg_logout()
+
+app = AioClock(lifespan=lifespan)
+
+@app.task(trigger=Every(seconds=5))
+async def update_message():
+    global tg_bot
+    print(datetime.now().strftime("%H:%M:%S"),"Updating")
+    tg_bot.fetch_new_positions()
+    tg_bot.update_message(tg_bot.pretty_data())
 
 asyncio.run(app.serve())
